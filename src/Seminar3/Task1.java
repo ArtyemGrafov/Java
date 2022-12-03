877e29868bdbb8d91f9cee7e08bfd137d01fa390
ArtyemGrafov <110446339+ArtyemGrafov@users.noreply.github.com>
2022-12-03 16:19:54 +0300

    Seminar3

--- src/Seminar3/Task1.java
+++ src/Seminar3/Task1.java
@@ -8,33 +8,28 @@ import java.util.Random;
 public class Task1 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] array = new int[100];
+        int[] array = new int[10];
 
         for (int i = 0; i < array.length; i++) array[i] = random.nextInt();
-
+        System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(sort(array)));
     }
 
-    public static int[] sort(int[] arr) {
-        int[] buffer1 = Arrays.copyOf(arr, arr.length);
-        int[] buffer2 = new int[arr.length];
-        int[] result = mergesortInner(buffer1, buffer2, 0, arr.length);
-        return result;
+    public static int[] sort(int[] array) {
+        return sortArray(array, new int[array.length], 0, array.length);
     }
 
-    public static int[] mergesortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
-        if (startIndex >= endIndex - 1) return buffer1;
+    public static int[] sortArray(int[] array1, int[] array2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) return array1;
 
-        // уже отсортирован.
         int middle = startIndex + (endIndex - startIndex) / 2;
-        int[] sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
-        int[] sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
+        int[] sorted1 = sortArray(array1, array2, startIndex, middle);
+        int[] sorted2 = sortArray(array1, array2, middle, endIndex);
 
-        // Слияние
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
-        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        int[] result = (sorted1 == array1) ? array2 : array1;
         while (index1 < middle && index2 < endIndex)
             result[destIndex++] = sorted1[index1] < sorted2[index2] ? sorted1[index1++] : sorted2[index2++];
 
